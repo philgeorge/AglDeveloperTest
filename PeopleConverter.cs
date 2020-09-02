555a1a00fc2fa0b2111badadc8e573615c3da478
Phil George <phil.george@virginpulse.com>
2020-09-02 19:39:37 +1000

    implemented data conversion to satisfy unit tests

--- PeopleConverter.cs
+++ PeopleConverter.cs
@@ -2,7 +2,7 @@
 using AglDeveloperTest.OutputModel;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AglDeveloperTest
 {
@@ -10,7 +10,40 @@ namespace AglDeveloperTest
     {
         public Model Convert(People people)
         {
-            return null;
+            if (people == null)
+            {
+                return null;
+            }
+
+            List<GenderAndCats> genders;
+            if (people.Owners == null)
+            {
+                genders = new List<GenderAndCats>();
+            }
+            else
+            {
+                genders = people.Owners.Select(owner => new GenderAndCats
+                {
+                    Gender = owner.Gender.ToString(),
+                    CatNames = MapPetsToCatNames(owner.Pets)
+                }).ToList();
+            }
+
+            return new Model { Genders = genders };
+        }
+
+        private IList<string> MapPetsToCatNames(ICollection<Pet> pets)
+        {
+            if (pets == null)
+            {
+                return new List<string>();
+            }
+            return pets.Where(p=> IsCat(p.Type)).Select(p => p.Name).ToList();
+        }
+
+        private bool IsCat(string petType)
+        {
+            return string.Equals(petType, "cat", StringComparison.OrdinalIgnoreCase);
         }
     }
 }

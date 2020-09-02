06a186d75b9f385181c05504d8e69245e96b4f55
Phil George <phil.george@virginpulse.com>
2020-09-02 17:52:29 +1000

    added model classes

---
+++ JsonModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AglDeveloperTest.InputModel
+{
+    // It's not necessarily a good choice to model Gender as an enum, since new string values could appear in the JSON without notice.
+    // However, if you have gaurantees about the set of permitted values, then it can help with the correctness and readability of your code.
+    public enum Gender { Male, Female }
+
+    public class Owner
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public ICollection<Pet> Pets { get; set; }
+    }
+
+    public class Pet
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class People
+    {
+        public ICollection<Owner> Owners { get; set; }
+    }
+}
---
+++ OutputModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AglDeveloperTest.OutputModel
+{
+    public class GenderAndCats
+    {
+        public string Gender { get; set; }
+        public ICollection<string> CatNames { get; set; }
+    }
+
+    public class OutputModel
+    {
+        public ICollection<GenderAndCats> Genders {get;set;}
+    }
+}

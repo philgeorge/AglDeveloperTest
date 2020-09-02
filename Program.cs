deb0838363015700f15b70aaace16e148af07768
Phil George <phil.george@virginpulse.com>
2020-09-02 17:51:28 +1000

    created console app

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AglDeveloperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // see http://agl-developer-test.azurewebsites.net/
+
+            Console.WriteLine("Hello World!");
+
+            //todo: create model for JSON response
+            //todo: create model for required output
+            //todo: method to transform
+            
+            //todo: unit tests            
+            //todo: DI for JSON web request and Console output
+
+            // HttpClientFactory for web request - note where to add auth, retries, etc.
+
+            // end-to-end integration tests
+        }
+    }
+}
